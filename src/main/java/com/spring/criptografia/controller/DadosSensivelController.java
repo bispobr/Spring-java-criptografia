2a7260aa6ae4c88fbc61c651b50c1339f04eb508
bispobr <156684382+bispobr@users.noreply.github.com>
2026-03-22 12:27:59 -0300

    feat: add novo endpoint e reestruturação

--- src/main/java/com/spring/criptografia/controller/DadosSensivelController.java
+++ src/main/java/com/spring/criptografia/controller/DadosSensivelController.java
@@ -1,14 +1,17 @@
 package com.spring.criptografia.controller;
 
-import com.spring.criptografia.model.DadosSensivel;
+import com.spring.criptografia.dto.RequisicaoDadosDTO;
+import com.spring.criptografia.dto.ResponseDTO;
 import com.spring.criptografia.service.DadosSensivelService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.PublicKey;
 import java.util.List;
 @Slf4j
 @RestController
@@ -21,27 +24,51 @@ public class DadosSensivelController {
     @GetMapping
     @Operation(description = "Endpoint responsável por listar todos os dados")
     @ApiResponse(responseCode = "200", description = "Listagem bem sucedida")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public List<DadosSensivel> listarTodos(){
+    public ResponseEntity<List<ResponseDTO>> listarTodos(){
         log.info("Requisição para listar dados recebida");
-        return dadosSensivelService.BuscaTodos();
+        return ResponseEntity.ok().body(dadosSensivelService.BuscaTodos());
+    }
+
+    @GetMapping("/{id}")
+    @Operation(description = "Endpoint responsável por listar dados por id")
+    @ApiResponse (responseCode = "200", description = "Listagem bem sucedida")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
+    @ApiResponse(responseCode = "500", description = "Erro interno")
+    public ResponseEntity<ResponseDTO> listarById(@PathVariable("id")Long id) {
+        log.info("solicitação de busca por id recebida;");
+        return dadosSensivelService.buscaPorID(id);
     }
 
     @PostMapping
     @Operation(description = "Endpoint responsável por salvar dados")
-    @ApiResponse(responseCode = "200", description = "dados salvo com sucesso")
+    @ApiResponse(responseCode = "201", description = "dados salvo com sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-     public  DadosSensivel criar(@RequestBody DadosSensivel data){
+    public ResponseEntity<ResponseDTO> criar(@RequestBody @Valid RequisicaoDadosDTO requisicao){
         log.info("Requisição para salvar dados sensíveis recebida");
-        return dadosSensivelService.Salvar(data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dadosSensivelService.Salvar(requisicao)) ;
+    }
+
+    @PutMapping("/{id}")
+    @Operation(description = "Endpoint responsável por atualizar dados")
+    @ApiResponse(responseCode = "200", description = "Produto atualizado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição, dados enviados não atendem os requisitos")
+    @ApiResponse(responseCode = "404", description = "Produto não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro interno")
+    public ResponseEntity<ResponseDTO> atualizarDados(@PathVariable("id")Long id , @RequestBody @Valid RequisicaoDadosDTO data) {
+        log.info("Solicitação de Atualização de dados recebida");
+        return dadosSensivelService.AtualizarDados(id, data);
     }
 
     @DeleteMapping("/{id}")
     @Operation(description = "Endpoint responsável por remover dados")
     @ApiResponse(responseCode = "200", description = "dados removidos com sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public void delete(@PathVariable Long id){
+    public ResponseEntity delete(@PathVariable Long id){
         log.info("Requisição para remover dados recebida");
-        dadosSensivelService.excluirById(id);
+        return dadosSensivelService.excluirById(id);
     }
 }
